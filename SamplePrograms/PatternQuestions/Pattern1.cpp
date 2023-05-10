9862556e2769293752b5549b5904e56e0ff357ae
Harshit Tiwari <tech.harshit.tiwari@gmail.com>
2023-05-10 13:36:36 +0530

    Added user input for ROWs and Columns in Patter1.cpp

--- SamplePrograms/PatternQuestions/Pattern1.cpp
+++ SamplePrograms/PatternQuestions/Pattern1.cpp
@@ -5,14 +5,21 @@
 ****
 ****
 ****
+Or the number or rows and columns user enters
 */
 #include <iostream>
 using namespace std;
 int main()
 {
-    for (int counter1 = 1; counter1 <= 5; counter1++)
+    int rows;
+    int columns;
+    cout<<"Enter number of ROWs";
+    cin >> rows;
+    cout<<"Enter number of coulumns";
+    cin >> columns;
+    for (int counter1 = 1; counter1 <= rows; counter1++)
     {
-        for (int counter2 = 1; counter2 <= 4; counter2++)
+        for (int counter2 = 1; counter2 <= columns; counter2++)
         {
             cout << "* ";
         }
