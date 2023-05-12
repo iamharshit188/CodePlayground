399818a0f496aeb038938d1ba95d48a928bfec05
Harshit Tiwari <tech.harshit.tiwari@gmail.com>
2023-05-12 22:10:59 +0530

    Added sample question and practise question

---
+++ C/PractiseQuestions/Question1.c
@@ -0,0 +1,9 @@
+//Write a program to calculate area of a square.
+#include <stdio.h>
+int main()
+{
+    int side ;
+    printf("Enter side of square : ");
+    scanf("&d" , side);
+    printf("Area of given square of side %d" , side , " is " , side * side );
+}
