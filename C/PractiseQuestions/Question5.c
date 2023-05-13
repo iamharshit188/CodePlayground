846d8de706bcd0851ee5280539acb4dd3c9e3a13
Harshit Tiwari <tech.harshit.tiwari@gmail.com>
2023-05-13 21:13:41 +0530

    Added more practise questions

---
+++ C/PractiseQuestions/Question5.c
@@ -0,0 +1,20 @@
+/* Practise Question:
+ Solve the following
+ 1) 5*2-2*3
+ 2) 5*2/2*3
+ 3) 5*(2/2)*3
+ 4) 5+2 /2*3
+ */
+#include <stdio.h>
+int main()
+{
+    int equation1 = 5 * 2 - 2 * 3;
+    int equation2 = 5 * 2 / 2 * 3;
+    int equation3 = 5 * (2 / 2) * 3;
+    int equation4 = 5 + 2 / 2 * 3;
+    printf("%d \n", equation1);
+    printf("%d \n", equation2);
+    printf("%d \n", equation3);
+    printf("%d \n", equation4);
+    return 0;
+}
