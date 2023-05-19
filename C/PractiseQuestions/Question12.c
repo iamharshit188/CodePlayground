82ea037a31ec7e88a7313b1f063aeb4bfd0c474a
Harshit Tiwari <iamharshit188@gmail.com>
2023-05-19 11:10:56 +0530

    Code cleaup

--- C/PractiseQuestions/Question12.c
+++ C/PractiseQuestions/Question12.c
@@ -30,23 +30,18 @@ int main()
     printf("Enter a three-digit integer: ");
     scanf("%d", &num);
     originalNum = num;
-
     while (originalNum != 0)
     {
         // remainder contains the last digit
         remainder = originalNum % 10;
-
         result += remainder * remainder * remainder;
-
         // removing last digit from the orignal number
         originalNum /= 10;
     }
-
     if (result == num)
         printf("%d is an Armstrong number.", num);
     else
         printf("%d is not an Armstrong number.", num);
-
     return 0;
 }
 // Taken from Programmiz : https://www.programiz.com/c-programming/examples/check-armstrong-number
