0879bb189d5af91ff89a62c08419ba7d84ad083d
Harshit Tiwari <iamharshit188@gmail.com>
2023-05-20 09:55:49 +0530

    Question13

---
+++ C/PractiseQuestions/Question13.c
@@ -0,0 +1,9 @@
+// Write a program to print number 0-100 using for loop.
+#include <stdio.h>
+int main()
+{
+    for (int counter = 0; counter <= 100; counter++)
+    {
+        printf("%d \n" , counter);
+    }
+} 
