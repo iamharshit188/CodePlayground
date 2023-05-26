7f8e0071f29bbf6a6bbba5a9b64563bb17c4f501
Harshit Tiwari <tech.harshit.tiwari@gmail.com>
2023-05-27 00:30:12 +0530

    Finallyyyyy without loop

--- C/PractiseQuestions/Question12.c
+++ C/PractiseQuestions/Question12.c
@@ -22,26 +22,45 @@
 //         printf("It is not an Armstrong number");
 //     }
 // }
-
 #include <stdio.h>
 int main()
 {
-    int num, originalNum, remainder, result = 0;
+    int num, num1, rem, res = 0;
     printf("Enter a three-digit integer: ");
     scanf("%d", &num);
-    originalNum = num;
-    while (originalNum != 0)
+    num1 = num;
+    if (num1 > 0)
     {
-        // remainder contains the last digit
-        remainder = originalNum % 10;
-        result += remainder * remainder * remainder;
+        if (num > 0)
+        {
+            if (num1 > 0)
+            {
+                rem = num1 % 10;
+
+                res += rem * rem * rem;
+
+                // removing last digit from the orignal number
+                num1 /= 10;
+            }
+            rem = num1 % 10;
+
+            res += rem * rem * rem;
+
+            // removing last digit from the orignal number
+            num1 /= 10;
+        }
+        rem = num1 % 10;
+
+        res += rem * rem * rem;
+
         // removing last digit from the orignal number
-        originalNum /= 10;
+        num1 /= 10;
     }
-    if (result == num)
+
+    if (res == num)
         printf("%d is an Armstrong number.", num);
     else
         printf("%d is not an Armstrong number.", num);
+
     return 0;
 }
-// Taken from Programmiz : https://www.programiz.com/c-programming/examples/check-armstrong-number
