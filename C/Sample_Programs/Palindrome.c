5acf968004a43fd9d2c892eb43ea1211ac916c8d
Harshit Tiwari <tech.harshit.tiwari@gmail.com>
2023-05-26 22:11:00 +0530

    Added Palindrome

---
+++ C/Sample_Programs/Palindrome.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+
+int isPalindrome(char str[])
+{
+    int length = strlen(str);
+    int i, j;
+
+    for (i = 0, j = length - 1; i < j; i++, j--)
+    {
+        if (str[i] != str[j])
+        {
+            return 0; // Not a palindrome
+        }
+    }
+
+    return 1; // Palindrome
+}
+
+int main()
+{
+    char str[100];
+
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    if (isPalindrome(str))
+    {
+        printf("Palindrome\n");
+    }
+    else
+    {
+        printf("Not a palindrome\n");
+    }
+
+    return 0;
+}
