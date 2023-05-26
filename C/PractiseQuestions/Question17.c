912d7429b3ac26b50b69bf1aaa6ec455c63c6449
Harshit Tiwari <tech.harshit.tiwari@gmail.com>
2023-05-26 22:11:46 +0530

    Added Question 17.c

---
+++ C/PractiseQuestions/Question17.c
@@ -0,0 +1,14 @@
+// Print multiplication of any number
+#include <stdio.h>
+int main()
+{
+    int number;
+    printf("Enter a number : ");
+    scanf("%d", &number);
+    for (int counter = 1; counter <= 10; counter++)
+    {
+        int temp = 0;
+        temp = number * counter ;
+        
+    }
+}
