a93600614a342c4ef3af9c62ac294eb120c9beee
Harshit Tiwari <tech.harshit.tiwari@gmail.com>
2023-05-29 18:52:58 +0530

    Added sample program

---
+++ C/Sample_Programs/functioc.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+//Function declaration / Prototype
+void printhello();
+int main()
+{
+	printhello();
+	printhello();
+	printhello();
+	printhello();
+	printhello();
+	printhello();
+	return 0;
+}
+
+void printhello()
+{
+	printf("Hello! \n");
+}
+
