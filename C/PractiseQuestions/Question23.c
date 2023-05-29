e53df9f886c207affd9f52b08441da1a9dbae16d
Harshit Tiwari <tech.harshit.tiwari@gmail.com>
2023-05-29 18:50:54 +0530

    Added question23

---
+++ C/PractiseQuestions/Question23.c
@@ -0,0 +1,19 @@
+//Write a program to print hello and good bye using function 
+#include<stdio.h>
+void printhello();
+void printbye();
+int main()
+{
+	printhello();
+	printbye();
+}
+void printhello()
+{
+	printf("Hello! , I am Harshit Tiwari \n");
+	printf("This is the first function \n");
+}
+void printbye()
+{
+	printf("See ya ! \n");
+	printf("This is the Second function \n");
+}
